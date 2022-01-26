e9da95a6e8f54e8cdf44d790b65ed3c723da160e
TheDawnCc <708855691@qq.com>
2022-01-26 15:57:51 +0800

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\qiaoxh2\\Desktop\\test\\docusaurus_test\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\qiaoxh2\\Desktop\\test\\docusaurus_test\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\qiaoxh2\\Desktop\\test\\docusaurus_test\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\admonitions.css"),
+  require("C:\\Users\\qiaoxh2\\Desktop\\test\\docusaurus_test\\my-website\\src\\css\\custom.css"),
+];
